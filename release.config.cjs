ddd18c687c4225c4bf69c9155391dff49399fd12
Mark Martirosian <hello@markmartirosian.com>
2023-01-14 15:11:23 +0100

    feat: update

--- release.config.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  branches: ['trunk'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/npm',
-    '@semantic-release/github'
-  ]
-}
