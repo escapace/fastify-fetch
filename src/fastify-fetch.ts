9e3570913799a838f168694621c4eb51848799e9
yyxi <178086254+yyxi@users.noreply.github.com>
2023-01-16 07:59:47 +0100

    fix: header conversion

--- src/fastify-fetch.ts
+++ src/fastify-fetch.ts
@@ -24,6 +24,116 @@ const inflate = promisify(_inflate)
 
 const supportedSchemas = new Set(['data:', 'http:', 'https:'])
 
+export interface NodeHeaders {
+  [header: string]: string | number | string[] | undefined
+}
+
+// https://github.com/vercel/next.js/blob/canary/packages/next/src/server/web/utils.ts
+
+/*
+  Set-Cookie header field-values are sometimes comma joined in one string. This splits them without choking on commas
+  that are within a single set-cookie field-value, such as in the Expires portion.
+  This is uncommon, but explicitly allowed - see https://tools.ietf.org/html/rfc2616#section-4.2
+  Node.js does this for every header *except* set-cookie - see https://github.com/nodejs/node/blob/d5e363b77ebaf1caf67cd7528224b651c86815c1/lib/_http_incoming.js#L128
+  React Native's fetch does this for *every* header, including set-cookie.
+
+  Based on: https://github.com/google/j2objc/commit/16820fdbc8f76ca0c33472810ce0cb03d20efe25
+  Credits to: https://github.com/tomball for original and https://github.com/chrusart for JavaScript implementation
+*/
+export function splitCookiesString(cookiesString: string) {
+  const cookiesStrings = []
+  let pos = 0
+  let start
+  let ch
+  let lastComma
+  let nextStart
+  let cookiesSeparatorFound
+
+  function skipWhitespace() {
+    while (pos < cookiesString.length && /\s/.test(cookiesString.charAt(pos))) {
+      pos += 1
+    }
+    return pos < cookiesString.length
+  }
+
+  function notSpecialChar() {
+    ch = cookiesString.charAt(pos)
+
+    return ch !== '=' && ch !== ';' && ch !== ','
+  }
+
+  while (pos < cookiesString.length) {
+    start = pos
+    cookiesSeparatorFound = false
+
+    while (skipWhitespace()) {
+      ch = cookiesString.charAt(pos)
+      if (ch === ',') {
+        // ',' is a cookie separator if we have later first '=', not ';' or ','
+        lastComma = pos
+        pos += 1
+
+        skipWhitespace()
+        nextStart = pos
+
+        while (pos < cookiesString.length && notSpecialChar()) {
+          pos += 1
+        }
+
+        // currently special character
+        if (pos < cookiesString.length && cookiesString.charAt(pos) === '=') {
+          // we found cookies separator
+          cookiesSeparatorFound = true
+          // pos is inside the next cookie, so back up and return it.
+          pos = nextStart
+          cookiesStrings.push(cookiesString.substring(start, lastComma))
+          start = pos
+        } else {
+          // in param ',' or param separator ';',
+          // we continue from that comma
+          pos = lastComma + 1
+        }
+      } else {
+        pos += 1
+      }
+    }
+
+    if (!cookiesSeparatorFound || pos >= cookiesString.length) {
+      cookiesStrings.push(cookiesString.substring(start, cookiesString.length))
+    }
+  }
+
+  return cookiesStrings
+}
+
+export function fromNodeHeaders(object: NodeHeaders): Headers {
+  const headers = new Headers()
+  for (const [key, value] of Object.entries(object)) {
+    const values = Array.isArray(value) ? value : [value]
+    for (const v of values) {
+      if (v !== undefined) {
+        headers.append(key, `${v}`)
+      }
+    }
+  }
+
+  return headers
+}
+
+export function toNodeHeaders(headers?: Headers): NodeHeaders {
+  const result: NodeHeaders = {}
+  if (headers != null) {
+    for (const [key, value] of headers.entries()) {
+      result[key] = value
+      if (key.toLowerCase() === 'set-cookie') {
+        result[key] = splitCookiesString(value)
+      }
+    }
+  }
+
+  return result
+}
+
 const decompressPayload = async (rawPayload: Buffer, headers: Headers) => {
   if (rawPayload.length === 0) {
     return undefined
@@ -123,7 +233,7 @@ export const fastifyFetch = fp<Options>(async (app, options = {}) => {
       ) {
         const response = await app.inject({
           url: request.url,
-          headers: Object.fromEntries(request.headers.entries()),
+          headers: toNodeHeaders(request.headers),
           payload: Buffer.from(await request.arrayBuffer()),
           method: method as
             | 'DELETE'
@@ -135,19 +245,7 @@ export const fastifyFetch = fp<Options>(async (app, options = {}) => {
             | 'PUT'
         })
 
-        const headers = new Headers()
-
-        for (const [key, values] of Object.entries(response.headers)) {
-          if (values === undefined) {
-            continue
-          } else if (Array.isArray(values)) {
-            for (const value of values) {
-              headers.set(key, value)
-            }
-          } else {
-            headers.set(key, `${values}`)
-          }
-        }
+        const headers = fromNodeHeaders(response.headers)
 
         const statusText = response.statusMessage
         const status = response.statusCode
