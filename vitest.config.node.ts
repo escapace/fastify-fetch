138bbd7e7fcd3f240d8107a50f44dfb8e28518ee
yyxi <178086254+yyxi@users.noreply.github.com>
2024-12-14 19:36:02 +0000

    chore: consistent test directory naming

--- vitest.config.node.ts
+++ vitest.config.node.ts
@@ -17,7 +17,7 @@ export default mergeConfig(
     },
     test: {
       environment: 'node',
-      include: ['{src,tests}/**/+([a-zA-Z0-9-])?(.node).{test,spec}.?(c|m)[jt]s?(x)'],
+      include: ['{src,test}/**/+([a-zA-Z0-9-])?(.node).{test,spec}.?(c|m)[jt]s?(x)'],
       name: 'node',
       sequence: {
         hooks: 'list',
